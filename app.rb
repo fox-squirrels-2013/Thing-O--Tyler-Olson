148f5bcaa9f2ec10ae2fd9c839280d98305cbf7d
TGOlson <tydotg@gmail.com>
2013-10-28 14:37:39 -0700

    luanch with ruby or rack

--- app.rb
+++ app.rb
@@ -2,11 +2,9 @@ require 'sinatra'
 require 'active_record'	
 require_relative './app/models/map'
 
-# class SinatraApp < Sinatra::Base
 
-# 	ActiveRecord::Base.establish_connection(adapter: 'postgresql')
+ActiveRecord::Base.establish_connection(adapter: 'postgresql')
 
-	get '/' do
-		erb :maps_page
-	end
-# end
+get '/' do
+	erb :maps_page
+end
