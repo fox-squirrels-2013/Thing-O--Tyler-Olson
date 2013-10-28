a9d0c6a39d6bf88e5739383b285e12855d804e14
TGOlson <tydotg@gmail.com>
2013-10-28 13:55:56 -0700

    style and view

--- main.rb
+++ main.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 
 get '/' do
-  "<h1>Hi this is not a map.</h1>
-  <p>But later, this page will be about maps.</p>
-  <ul><li>It will be cool - you will be able to see states, and cities and other places you might want to go...</li>
-  <li>...but right now it is definitely not a map.</li></ul>"
+  # "<h1>Hi this is not a map.</h1>
+  # <p>But later, this page will be about maps.</p>
+  # <ul><li>It will be cool - you will be able to see states, and cities and other places you might want to go...</li>
+  # <li>...but right now it is definitely not a map.</li></ul>"
+
+  erb :maps
 end
